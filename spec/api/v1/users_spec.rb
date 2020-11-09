22ed4fdfd6d79ccb5b7c1b37fc715b2405abe621
Daniel Lessenden <dless27@gmail.com>
2020-11-09 17:57:44 -0600

    Create new test to pass user_params to controller

--- spec/api/v1/users_spec.rb
+++ spec/api/v1/users_spec.rb
@@ -2,23 +2,34 @@ require 'rails_helper'
 
 describe 'Creates a new user API' do
   it 'can post to create a new user ' do
-    body = {
-              email: "whatever@example.com",
-              password: "password",
-              password_confirmation: "password"
-              }
 
+    user_params = ({
+                      email: 'whatever@example.com',
+                      password: 'password',
+                      password_confirmation: 'password'
+                    })
+      headers = {'CONTENT_TYPE' => 'application/json'}
+      post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+      json = JSON.parse(response.body, symbolize_names: true)
+      user = User.last
 
-    conn = Faraday.new(url: 'https://api/v1')
-    response = conn('/users').post do |request|
-     request.body = body
-    end
+    # body = {
+    #           email: "whatever@example.com",
+    #           password: "password",
+    #           password_confirmation: "password"
+    #           }
+    #
+    #
+    # conn = Faraday.new(url: 'https://api/v1')
+    # response = conn('/users').post do |request|
+    #  request.body = body
+    # end
 
-    binding.pry
+    # binding.pry
     # post "/api/v1/users?#{params}"
     # expect(response).to be_successful
 
-    json = JSON.parse(response.body)
+    # json = JSON.parse(response.body)
       # post '/api/v1/users'
       # Content-Type: application/json
       # Accept: application/json
