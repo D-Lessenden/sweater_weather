d05b2482f4ca03dd7338b49a3c88b09088dfda57
Daniel Lessenden <dless27@gmail.com>
2020-11-09 10:47:35 -0600

    Add a serializer

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::TrailsController < ApplicationController
   def show
     coords = MapFacade.return_coords(params[:location])
-    binding.pry
-    # render json: ForecastSerializer.new(ForecastFacade.return_forecast(coords))
+
+    conn = Faraday.new('https://www.hikingproject.com')
+    response = conn.get("/data/get-trails?lat=#{coords[0]}&lon=#{coords[1]}&maxDistance=30&key=#{ENV['HIKING-API']}")
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    render json: TrailsSerializer.new(json)
   end
 end
