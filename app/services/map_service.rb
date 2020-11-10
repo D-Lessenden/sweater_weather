82f7ceeb1738df5a324fd215771b131e3b8fe030
Daniel Lessenden <dless27@gmail.com>
2020-11-10 11:55:10 -0600

    Create new method to convert time

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -19,4 +19,15 @@ class MapService
     coords << lon
     coords
   end
+
+  def self.route(start, finish)
+   conn = Faraday.new('https://www.mapquestapi.com')
+   response = conn.get("/directions/v2/route?key=#{ENV['MAP-API']}&from=#{start}&to=#{finish}&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false")
+   json = JSON.parse(response.body, symbolize_names: true)
+ end
+
+ def self.convert_time(time)
+   array = time.split(":")
+   converted = array[0] + ' ' + 'hours' + ' ' + array[1] + ' ' + 'min'
+ end
 end
