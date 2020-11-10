313271e94f926eae703bf7228dacc544102114f2
Daniel Lessenden <dless27@gmail.com>
2020-11-10 16:33:30 -0600

    Add check for api validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,15 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of :password}
     it {should validate_presence_of :password_confirmation}
     it {should validate_uniqueness_of :email}
+    it {should validate_presence_of :api_key}
   end
 
   it 'can create user' do
     user_params = {
                     email: 'whatever@example.com',
                     password: 'password',
-                    password_confirmation: 'password'
+                    password_confirmation: 'password',
+                    api_key: 'feiqwjhf987234rjf34r'
                   }
 
     new_user = User.create(user_params)
