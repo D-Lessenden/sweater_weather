9a9b0f58498dd5374499e1b7c28d75490676cddc
Daniel Lessenden <dless27@gmail.com>
2020-11-10 17:36:10 -0600

    Add render json error messages for creating a user if required params are not provided

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    binding.pry 
+    user = User.new(user_params)
+    if user_params[:password].nil?
+      render json: {error: "Password is a required field"}, status: :not_acceptable
+    elsif user_params[:password_confirmation].nil?
+      render json: {error: "Password confirmation is a required field"}, status: :not_acceptable
+    elsif user_params[:email].nil?
+      render json: {error: "Email is a required field"}, status: :not_acceptable
+    elsif user_params[:password] != user_params[:password_confirmation]
+      render json: {error: "Passwords do not match"}, status: :not_acceptable
+    else
+    user.save
+    render json: UsersSerializer.new(user)
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
+params[:email]
