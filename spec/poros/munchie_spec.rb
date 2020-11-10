1d46635c8303b915fe5f594d16f10627f39d94f4
Daniel Lessenden <dless27@gmail.com>
2020-11-10 11:33:11 -0600

    Create test to ensure the poro is being created

---
+++ spec/poros/munchie_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+describe Munchie do
+  it 'can create an object' do
+    coords = MapFacade.return_coords('pueblo,co')
+    route = MapService.route('puebloe,co', 'denver,co')
+    forecast = ForecastFacade.return_forecast(coords)
+
+    conn = Faraday.new(url: 'https://api.yelp.com') do |f|
+      f.headers['Authorization'] = "Bearer #{ENV['YELP-API']}"
+    end
+    response = conn.get("/v3/businesses/search?term=chinese&latitude=#{coords[0]}&longitude=#{coords[1]}&limit=1")
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    restaurant_route = Munchie.new(forecast: forecast, restaurant: json, destination: route)
+
+    expect(restaurant_route.destination).to have_key(:route)
+    expect(restaurant_route.forecast).to have_key(:current)
+    expect(restaurant_route.restaurant).to have_key(:businesses)
+  end
+end
