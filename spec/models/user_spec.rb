f2860450ee84da4c52ddc1d99a5b3163aa636a47
Daniel Lessenden <dless27@gmail.com>
2020-11-10 16:07:45 -0600

    Add validations and test that user can be created

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it {should validate_presence_of :email}
+    it {should validate_presence_of :password}
+    it {should validate_presence_of :password_confirmation}
+    it {should validate_uniqueness_of :email}
+  end
+
+  it 'can create user' do
+    user_params = {
+                    email: 'whatever@example.com',
+                    password: 'password',
+                    password_confirmation: 'password'
+                  }
+
+    new_user = User.create(user_params)
+
+    expect(new_user.email).to eq('whatever@example.com')
+  end
 end
