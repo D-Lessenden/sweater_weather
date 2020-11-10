34dd54bd82f66f38f7d7d7be43b71c3dbc7bca09
Daniel Lessenden <dless27@gmail.com>
2020-11-10 16:07:28 -0600

    Add validations and test that user can be created

--- spec/api/v1/users_spec.rb
+++ spec/api/v1/users_spec.rb
@@ -12,33 +12,5 @@ describe 'Creates a new user API' do
       post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
       json = JSON.parse(response.body, symbolize_names: true)
       user = User.last
-
-    # body = {
-    #           email: "whatever@example.com",
-    #           password: "password",
-    #           password_confirmation: "password"
-    #           }
-    #
-    #
-    # conn = Faraday.new(url: 'https://api/v1')
-    # response = conn('/users').post do |request|
-    #  request.body = body
-    # end
-
-    # binding.pry
-    # post "/api/v1/users?#{params}"
-    # expect(response).to be_successful
-
-    # json = JSON.parse(response.body)
-      # post '/api/v1/users'
-      # Content-Type: application/json
-      # Accept: application/json
-      #
-      # {
-      #   "email": "whatever@example.com",
-      #   "password": "password"
-      #   "password_confirmation": "password"
-      # }
-
   end
 end
