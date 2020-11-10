8d5d3af22c881ee98c8eab62bb97d69fff2c0ade
Daniel Lessenden <dless27@gmail.com>
2020-11-10 10:53:35 -0600

    Create tests for all rest. attributes

--- spec/api/v1/munchies_spec.rb
+++ spec/api/v1/munchies_spec.rb
@@ -5,7 +5,6 @@ describe 'Yelp API returns info FE team needs' do
     get "/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese"
     expect(response).to be_successful
     restaurant = JSON.parse(response.body, symbolize_names: true)[:data]
-    binding.pry
 
     expect(restaurant[:id]).to eq(nil)
     expect(restaurant[:type]).to eq('munchie')
@@ -19,10 +18,7 @@ describe 'Yelp API returns info FE team needs' do
 
     expect(restaurant[:attributes][:restaurant]).to have_key(:address)
     expect(restaurant[:attributes][:restaurant][:address]).to be_a(String)
-
-
-
-
+    expect(restaurant[:attributes][:restaurant][:address]).to eq("315 W 4th St Pueblo, CO 81003")
 
   end
 end
