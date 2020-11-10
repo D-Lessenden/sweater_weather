807c659a8143405d78d5d1a8c3d94a4d1f10560d
Daniel Lessenden <dless27@gmail.com>
2020-11-10 11:54:50 -0600

    Test new method to convert time

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -7,4 +7,20 @@ describe 'Map Service' do
     expect(coords[0]).to eq(39.738453)
     expect(coords[1]).to eq(-104.984853)
   end
+
+  it 'can return a route from point a to point b' do
+    point_a = 'denver,co'
+    point_b = 'pueblo,co'
+
+    route = MapService.route(point_a, point_b)
+    expect(route).to have_key(:route)
+    expect(route[:route]).to have_key(:formattedTime)
+    expect(route[:route][:formattedTime]).to eq("01:43:57")
+  end
+
+  it 'converts time to h and m' do
+    time = "01:43:57"
+    converted = MapService.convert_time(time)
+    expect(converted).to eq("01 hours 43 min")
+  end
 end
