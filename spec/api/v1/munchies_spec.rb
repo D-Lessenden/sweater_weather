721a922525da0730040b040ea4b2bf017a32b437
Daniel Lessenden <dless27@gmail.com>
2020-11-10 10:42:21 -0600

    Create the test for da munchies

---
+++ spec/api/v1/munchies_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'Yelp API returns info FE team needs' do
+  it 'gives restaurant for selected location' do
+    get "/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese"
+    expect(response).to be_successful
+    restaurant = JSON.parse(response.body, symbolize_names: true)[:data]
+    binding.pry
+
+    expect(restaurant[:id]).to eq(nil)
+    expect(restaurant[:type]).to eq('munchie')
+
+    expect(restaurant[:attributes]).to have_key(:restaurant)
+    expect(restaurant[:attributes][:restaurant].keys.count).to eq(2)
+
+    expect(restaurant[:attributes][:restaurant]).to have_key(:name)
+    expect(restaurant[:attributes][:restaurant][:name]).to be_a(String)
+    expect(restaurant[:attributes][:restaurant][:name]).to eq("China Lantern")
+
+    expect(restaurant[:attributes][:restaurant]).to have_key(:address)
+    expect(restaurant[:attributes][:restaurant][:address]).to be_a(String)
+
+
+
+
+
+  end
+end
