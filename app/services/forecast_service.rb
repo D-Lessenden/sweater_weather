c4293edcf1ad5f7d22ba0023ee6d7a0235ee6a17
Daniel Lessenden <dless27@gmail.com>
2020-11-10 23:00:00 -0600

    Create the weather at eta method

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -13,4 +13,16 @@ class ForecastService
     end
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.weather_at_eta(coords, time)
+    response = conn.get("/data/2.5/onecall") do |f|
+      f.params[:lat] = coords[0]
+      f.params[:lon] = coords[1]
+      f.params[:exclude] = 'minutely'
+      f.params[:units] = 'imperial'
+      f.params[:appid] = ENV['WEATHER-API']
+    end
+    json = JSON.parse(response.body, symbolize_names: true)
+    json[:hourly].find { |hour| hour if hour[:dt] == time }
+  end
 end
