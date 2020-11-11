d407f6c2e90a416581eb32debd1d5a681e83b0e1
Daniel Lessenden <dless27@gmail.com>
2020-11-10 20:20:33 -0600

    Create a test for facade abstraction

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -7,4 +7,15 @@ describe 'Map Facade' do
     expect(coords[0]).to eq(39.738453)
     expect(coords[1]).to eq(-104.984853)
   end
+
+  it 'can return a route for two locations' do
+    start = 'Denver, CO'
+    finish = 'Pueblo, CO'
+    route = MapFacade.route(start, finish)
+    expect(route).to have_key(:route)
+    expect(route[:route]).to have_key(:formattedTime)
+    expect(route[:route][:formattedTime]).to eq("01:43:57")
+    expect(route[:route]).to have_key(:time)
+    expect(route[:route][:time]).to eq(6237)
+  end
 end
