f9134db8f2954adef116f26eeb91fbcec25e2ae0
Daniel Lessenden <dless27@gmail.com>
2020-11-11 01:13:15 -0600

    Delete extra images that were not needed

--- app/serializers/backgrounds_serializer.rb
+++ app/serializers/backgrounds_serializer.rb
@@ -8,8 +8,6 @@ class BackgroundsSerializer
       image_url_raw: data[:json][:results][0][:urls][:raw],
       image_url_full: data[:json][:results][0][:urls][:full],
       image_url_regular: data[:json][:results][0][:urls][:regular],
-      image_url_small: data[:json][:results][0][:urls][:small],
-      image_url_thumb: data[:json][:results][0][:urls][:thumb],
       credit: {
                 creator: data[:json][:results][0][:user][:username],
                 portfolio: data[:json][:results][0][:user][:portfolio_url]
