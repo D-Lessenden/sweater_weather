4bd1021e64810e2d1535984ee8af2aba1a4155e5
Daniel Lessenden <dless27@gmail.com>
2020-11-11 00:54:44 -0600

    Nest credit within the image block

--- app/serializers/backgrounds_serializer.rb
+++ app/serializers/backgrounds_serializer.rb
@@ -4,15 +4,16 @@ class BackgroundsSerializer
 
   attributes :image do |data|
     {
-      image_url_raw: data[:urls][:raw],
-      image_url_full: data[:urls][:full],
-      image_url_regular: data[:urls][:regular],
-      image_url_small: data[:urls][:small],
-      image_url_thumb: data[:urls][:thumb],
-      creator: data[:user][:username],
-      portfolio: data[:user][:portfolio_url]
+      location: data[:location],
+      image_url_raw: data[:json][:results][0][:urls][:raw],
+      image_url_full: data[:json][:results][0][:urls][:full],
+      image_url_regular: data[:json][:results][0][:urls][:regular],
+      image_url_small: data[:json][:results][0][:urls][:small],
+      image_url_thumb: data[:json][:results][0][:urls][:thumb],
+      credit: {
+                creator: data[:json][:results][0][:user][:username],
+                portfolio: data[:json][:results][0][:user][:portfolio_url]
+              }
     }
   end
-
-
 end
