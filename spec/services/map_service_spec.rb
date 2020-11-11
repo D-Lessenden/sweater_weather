869fda8f6524372456a5c98814433a0d3de22693
Daniel Lessenden <dless27@gmail.com>
2020-11-10 20:20:52 -0600

    Create a test for service abstraction

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -7,4 +7,16 @@ describe 'Map Service' do
     expect(coords[0]).to eq(39.738453)
     expect(coords[1]).to eq(-104.984853)
   end
+
+  it 'can return a route from point a to point b' do
+   point_a = 'denver,co'
+   point_b = 'pueblo,co'
+   route = MapService.route(point_a, point_b)
+   expect(route).to have_key(:route)
+   expect(route[:route]).to have_key(:formattedTime)
+   expect(route[:route][:formattedTime]).to eq("01:43:57")
+   expect(route[:route]).to have_key(:time)
+   expect(route[:route][:time]).to eq(6237)
+   # expect(route[:route][:realTime]).to eq(6395) this is current time
+ end
 end
