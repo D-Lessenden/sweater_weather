c3e2df1775eec678a14daaea8abe60b64d417a5e
Daniel Lessenden <dless27@gmail.com>
2020-11-10 20:21:52 -0600

    Create abstraction for grabbing a route between two locations

--- app/facades/map_facade.rb
+++ app/facades/map_facade.rb
@@ -2,4 +2,8 @@ class MapFacade
   def self.return_coords(location)
     coords = MapService.return_coords(location)
   end
+
+  def self.route(start, finish)
+    route = MapService.route(start, finish)
+  end
 end
