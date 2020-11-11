b14c98f84be91de0d39a81b60f36012cca9e3b5c
Daniel Lessenden <dless27@gmail.com>
2020-11-10 20:21:34 -0600

    Create abstraction for grabbing a route between two locations

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -19,4 +19,19 @@ class MapService
     coords << lon
     coords
   end
+
+  def self.route(start, finish)
+   response = conn.get("/directions/v2/route") do |f|
+     f.params[:key] = ENV['MAP-API']
+     f.params[:from] = start
+     f.params[:to] = finish
+     f.params[:outFormat] = 'json'
+     f.params[:ambiguities] = 'ignore'
+     f.params[:routeType] = 'fastest'
+     f.params[:doReverseGeocode] = 'false'
+     f.params[:enhancedNarrative] = 'false'
+     f.params[:avoidTimedConditions] = 'false'
+   end
+   json = JSON.parse(response.body, symbolize_names: true)
+ end
 end
